eb451bf3b2cc7dfb63af9dc2532d04fd85234834
Aleksandr Demidenko <sashatro@mail.ru>
2024-07-20 18:30:31 +0300

    feat: Add auto navigate to /beans

--- src/components/Beans/Beans.tsx
+++ src/components/Beans/Beans.tsx
@@ -80,6 +80,8 @@ const Beans: FC = () => {
 
   //Initial fetch
   useEffect(() => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    console.log(scrollTop, clientHeight, scrollHeight);
     if (!timesRender.current && !beans.length) {
       timesRender.current = 1;
       dispatch(setBeans({ items: [] }));
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,50 @@
 import clsx from "clsx";
 import { useEffect, useRef, useState, type FC } from "react";
 import style from "./Home.module.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import Footer from "../Footer/Footer";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+  selectAllBeans,
+  selectOffset,
+  setBeans,
+  setOffset,
+  setTotalPages,
+} from "../../app/reducers/beanSlice";
+import { fetchData } from "../../api/fetchData";
 
 const Home: FC = () => {
+  const navigate = useNavigate();
   const [isHover, setIsHover] = useState(false);
   const header = useRef(null);
   const tl = useRef(gsap.timeline());
+
+  const timesRender = useRef(0);
+  const dispatch = useAppDispatch();
+  const beans = useAppSelector(selectAllBeans);
+  const PORTION_OF_ITEMS = 12;
+  const offset = useAppSelector(selectOffset);
+
+  //Initial fetch
+  useEffect(() => {
+    if (!timesRender.current && !beans.length) {
+      timesRender.current = 1;
+      dispatch(setBeans({ items: [] }));
+      dispatch(setOffset());
+      fetchData
+        .getData(offset, PORTION_OF_ITEMS)
+        .then(res => {
+          dispatch(setBeans({ items: res.items }));
+          dispatch(setTotalPages({ totalPages: res.totalPages }));
+          navigate("/beans");
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }, [beans.length, dispatch, navigate, offset]);
 
   useEffect(() => {
     if (!isHover) {
